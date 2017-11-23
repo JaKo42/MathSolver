e23e52e7a509439a9c6419bd251d133920db7141
JaKo42 <kokkinos.Joannis@gmail.com>
2017-11-23 10:40:04 +0100

    Initial commit

---
+++ src/core/Calculation.java
@@ -0,0 +1,21 @@
+package core;
+
+public class Calculation {
+
+    public static int divide(int dig1, int dig2){
+        int solution = dig1/dig2;
+        return solution;
+    }
+    public static int multiply(int dig1, int dig2){
+        int solution = dig1*dig2;
+        return solution;
+    }
+    public static int subtract(int dig1, int dig2){
+        int solution = dig1-dig2;
+        return solution;
+    }
+    public static int add(int dig1, int dig2){
+        int solution = dig1+dig2;
+        return solution;
+    }
+}
---
+++ src/core/test.java
@@ -0,0 +1,68 @@
+package core;
+import java.util.Scanner;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+
+public class test extends Application{
+    Scanner digitReader = new Scanner(System.in);
+
+
+    @Override
+    public void start(Stage stage) throws Exception{
+//       Erstellung der Labels
+        GridPane grid = new GridPane();       //Grundlegende Struktur
+            grid.setPadding(new Insets(100,10,10,10));
+            grid.setHgap(5);
+            grid.setVgap(5);
+        Label titel1 = new Label("Mathe Program"); //Erstes Label mit Titel
+            grid.add(titel1,3,0,1,1 );
+        Label description1= new Label("Geben Sie zwei Zahlen ein"); //Zweites Label mit Anweisung/Beschreibung
+            grid.add(description1,3,3,1,1);
+        TextField digitField = new TextField("Erste Zahl");//Erstes Label mit Zahlenfeld
+            grid.add(digitField,2,8,1,1);
+            digitField.setPromptText("Erste Zahl eingeben");
+            digitField.setPrefColumnCount(10);
+            digitField.getText();
+
+        Label digitField2 = new Label("Zweite Zahl");//Zweites Label mit Zahlenfeld
+            grid.add(digitField2,6,8,1,1);
+
+        Scene scene = new Scene(grid, 600, 400); //Erstellung der Scene
+        stage.setTitle("Math Program");
+        stage.setScene(scene);
+
+        //Erstellung der Buttons
+        Button add = new Button();
+            add.setText("Addieren");
+            add.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent event)  {
+
+//                    Calculation.add();
+                }
+            });
+        grid.add(add,2,12,1,1);
+
+
+        stage.show();
+
+    }
+
+    @Override
+    public void stop() throws Exception {
+            }
+
+    public static void main(String[] args) {
+        launch(args);
+
+
+    }
+}
