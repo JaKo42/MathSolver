2bcb575f9e4942c22bf556e8ebbe3d8ee476a555
JaKo42 <kokkinos.Joannis@gmail.com>
2017-11-23 13:14:32 +0100

    Add add Button Function

--- src/core/test.java
+++ src/core/test.java
@@ -17,7 +17,7 @@ public class test extends Application{
 
     @Override
     public void start(Stage stage) throws Exception{
-//       Erstellung der Labels
+//      Erstellung der Labels
         GridPane grid = new GridPane();       //Grundlegende Struktur
             grid.setPadding(new Insets(10,10,10,10));
             grid.setHgap(5);
@@ -31,19 +31,19 @@ public class test extends Application{
             digitField.setPromptText("Erste Zahl eingeben");
             digitField.setPrefColumnCount(10);
             digitField.getText();
-//TODO: Zahlen sollen übergeben werden
+            //TODO: Zahlen sollen übergeben werden
         TextField digitField2 = new TextField("Zweite Zahl");//Zweites Label mit Zahlenfeld
             grid.add(digitField2,6,8,1,1);
             digitField2.setPromptText("Zweite Zahl eingeben");
             digitField2.setPrefColumnCount(10);
-            digitField2.getText();
-//TODO: Zahlen sollen übergeben werden
+            digitField2.get();
+            //TODO: Zahlen sollen übergeben werden
 
         TextField solutionField = new TextField("Ergebnis");//Textfeld mit der Lösung
-        grid.add(solutionField,12,0,1,1);
-        solutionField.setPromptText("Ergebnis");
-        solutionField.setPrefColumnCount(10);
-//TODO:Ergebnis soll ausgegeben werden
+            grid.add(solutionField,12,0,1,1);
+            solutionField.setPromptText("Ergebnis");
+            solutionField.setPrefColumnCount(10);
+             //TODO:Ergebnis soll ausgegeben werden
 
 
         Scene scene = new Scene(grid, 600, 400); //Erstellung der Scene
@@ -55,9 +55,19 @@ public class test extends Application{
             add.setText("Addieren");
             add.setOnAction(new EventHandler<ActionEvent>() {
                 @Override
-                public void handle(ActionEvent event)  {
-//TODO:Funktion hinzufügen
-//                    Calculation.add();
+                public void handle(ActionEvent event) throws NumberFormatException {
+                try {
+
+
+                    if ((digitField.getText() != null && digitField.getText() != null && !digitField.getText().isEmpty() && !digitField2.getText().isEmpty() ))
+                    Calculation.add(Integer.parseInt(digitField.getText()),Integer.parseInt(digitField2.getText()));
+                }
+                catch (NumberFormatException invalidEntry){
+                    //TODO: Fehlerbehandlung einfügen
+                }
+
+                //TODO:Funktion hinzufügen
+
                 }
             });
         grid.add(add,2,12,1,1);
