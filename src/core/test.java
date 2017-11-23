a473192701028425d11a09eb89945c42f33bb6d2
JaKo42 <kokkinos.Joannis@gmail.com>
2017-11-23 15:43:02 +0100

    Add aditional Buttons and new TODO's

--- src/core/test.java
+++ src/core/test.java
@@ -5,11 +5,9 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
-import javafx.scene.control.Button;
 
 public class test extends Application{
     Scanner digitReader = new Scanner(System.in);
@@ -26,13 +24,13 @@ public class test extends Application{
             grid.add(titel1,3,0,1,1 );
         Label description1= new Label("Geben Sie zwei Zahlen ein"); //Zweites Label mit Anweisung/Beschreibung
             grid.add(description1,3,3,1,1);
-        TextField digitField = new TextField("Erste Zahl");//Erstes Label mit Zahlenfeld
+        TextField digitField = new TextField("Erste Zahl");//Erstes Textfeld mit Zahlenfeld
             grid.add(digitField,2,8,1,1);
             digitField.setPromptText("Erste Zahl eingeben");
             digitField.setPrefColumnCount(10);
             digitField.getText();
 
-        TextField digitField2 = new TextField("Zweite Zahl");//Zweites Label mit Zahlenfeld
+        TextField digitField2 = new TextField("Zweite Zahl");//Zweites Textfeld mit Zahlenfeld
             grid.add(digitField2,6,8,1,1);
             digitField2.setPromptText("Zweite Zahl eingeben");
             digitField2.setPrefColumnCount(10);
@@ -43,6 +41,7 @@ public class test extends Application{
             grid.add(solutionField,12,0,1,1);
             solutionField.setPromptText("Ergebnis");
             solutionField.setPrefColumnCount(10);
+            //TODO:Titel "Ergebnis" vor das Textfeld einfügen
 
 
 
@@ -60,15 +59,74 @@ public class test extends Application{
 
 
                     if ((digitField.getText() != null && digitField.getText() != null && !digitField.getText().isEmpty() && !digitField2.getText().isEmpty() ))
-                   solutionField.setText(""+(Calculation.add(Integer.parseInt(digitField.getText()),Integer.parseInt(digitField2.getText()))));
+                        solutionField.setText(""+(Calculation.add(Integer.parseInt(digitField.getText()),Integer.parseInt(digitField2.getText()))));
                 }
                 catch (NumberFormatException invalidEntry){
-                    //TODO: Fehlerbehandlung einfügen
+                    Alert warning = new Alert(Alert.AlertType.ERROR, "Bitte Felder mit Zahlen füllen",ButtonType.OK);
+                    warning.showAndWait();
                 }
 
                 }
             });
+        Button sub = new Button();
+        sub.setText("Subtrahieren");
+        sub.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) throws NumberFormatException {
+                try {
+
+
+                    if ((digitField.getText() != null && digitField.getText() != null && !digitField.getText().isEmpty() && !digitField2.getText().isEmpty() ))
+                        solutionField.setText(""+(Calculation.subtract(Integer.parseInt(digitField.getText()),Integer.parseInt(digitField2.getText()))));
+                }
+                catch (NumberFormatException invalidEntry){
+                    Alert warning = new Alert(Alert.AlertType.ERROR, "Bitte Felder mit Zahlen füllen",ButtonType.OK);
+                    warning.showAndWait();
+                }
+
+            }
+        });
+        Button div = new Button();
+        div.setText("Dividieren");
+        div.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) throws NumberFormatException {
+                try {
+                //TODO:Dividieren durch 0 abfangen
+
+                    if ((digitField.getText() != null && digitField.getText() != null && !digitField.getText().isEmpty() && !digitField2.getText().isEmpty() ))
+                        solutionField.setText(""+(Calculation.divide(Integer.parseInt(digitField.getText()),Integer.parseInt(digitField2.getText()))));
+                }
+                catch (NumberFormatException invalidEntry){
+                    Alert warning = new Alert(Alert.AlertType.ERROR, "Bitte Felder mit Zahlen füllen",ButtonType.OK);
+                    warning.showAndWait();
+                }
+
+            }
+        });
+        Button mult = new Button();
+        mult.setText("Multiplizieren");
+        mult.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) throws NumberFormatException {
+                try {
+
+
+                    if ((digitField.getText() != null && digitField.getText() != null && !digitField.getText().isEmpty() && !digitField2.getText().isEmpty() ))
+                        solutionField.setText(""+(Calculation.multiply(Integer.parseInt(digitField.getText()),Integer.parseInt(digitField2.getText()))));
+                }
+                catch (NumberFormatException invalidEntry){
+                    Alert warning = new Alert(Alert.AlertType.ERROR, "Bitte Felder mit Zahlen füllen",ButtonType.OK);
+                    warning.showAndWait();
+                }
+
+            }
+        });
+
+
+
         grid.add(add,2,12,1,1);
+        //TODO:Buttons richtig anordnen
 
 
         stage.show();
